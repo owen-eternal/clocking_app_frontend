95c31ac1660d5dd6c031039ddb5cadf6a206aad2
owen-eternal <poesylyrics@gmail.com>
2021-01-17 15:55:27 +0200

    adding map functionaliy

--- script.js
+++ script.js
@@ -1,52 +1,43 @@
-// const canvas = document.getElementById('canvas');
-//  
-// const ctx = canvas.getContext("2d");
-
-// const setupCanvas = ()=> {
-//     ctx.fillStyle = "#ccc";
-//     ctx.rect(0, 0, canvas.width, canvas.height);
-//     ctx.fill(); 
-//     ctx.fillStyle = "Blue";
-//     ctx.font = "35px serif";
-//     ctx.fillText("decode this bitch", 30, 60);
-// }
-
-if ("geolocation" in navigator){
-  console.log('available')
-  navigator.geolocation.getCurrentPosition( position => {
-    const { latitude, longitude }  = position.coords;
-    console.log(latitude, longitude)
-  });
-} else{l
-  console.log('unavailable')
-}
-
-// function init() {
-
-//     const base64 = canvas.toDataURL().split(",")[1];
-
-//     const data = {
-//       "generated_at" : new Date().toISOString(),
-//       "png": base64, 
-//     };
-
-//     const options = {
-//         method: 'POST',
-//         cache: 'no-cache',
-//         body: JSON.stringify(data),
-//         headers: { "Content-Type" : "application/json"}
-//     }
-    
-//     fetch('http://127.0.0.1:5000//api/1.0/clockings/upload', options);
-   
-// }
-
-// setupCanvas()
-// btnUpload.addEventListener('click', init)
+const btnLocation = document.getElementById("location");
 
 
+btnLocation.addEventListener('click', ()=>{
+    let platform = new H.service.Platform({
+        'apikey': 'MW6aHeGhrIuXS85D7cpKDgJaalCovbPx40nbSWwxJ60'
+        });
 
+    let geocoderService = platform.getGeocodingService();
 
+    if (navigator.geolocation) {
 
+        navigator.geolocation.getCurrentPosition(position =>{
+            const { latitude, longitude } = position.coords
+
+            //Get address
+            geocoderService.reverseGeocode(
+                {
+                mode: "retrieveAddresses",
+                maxresults: 1,
+                prox: -26.2129 + "," + 28.1638
+                }, data => {
+                    const address = data.Response.View[0].Result[0].Location.Address.Label
+                    document.getElementById("Address").value = address;
+                });
+
+            //create the map
+            let map = new H.Map(
+                document.getElementById('mapContainer'),
+                platform.createDefaultLayers().vector.normal.map,
+                {
+                zoom: 10,
+                center: { lat: latitude, lng: longitude }
+                });
+                const marker = new H.map.Marker({lat: latitude, lng: longitude});
+                map.addObject(marker);
+        });
+    } else {
+        console.log("not available");
+    }  
+})
 
 
